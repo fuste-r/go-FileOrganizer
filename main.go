975aacc54a0a4016df83ae86c2fc57f849c571d2
fuste-r <cjbookheimer@gmail.com>
2026-08-26 10:06:59 -0400

    add file extension detection

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -15,22 +17,35 @@ func main() {
 	}
 
 	entries, err := os.ReadDir(os.Args[1])
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, entry := range entries {
+		ext := strings.ToLower(filepath.Ext(entry.Name()))
 		fmt.Print(entry.Name())
 
 		if entry.IsDir() {
 			fmt.Printf("  -   Directory\n")
-			// } else {
-			// 	info, err := entry.Info()
-			// 	if err != nil {
-			// 		log.Fatal(err)
-			// 	}
-			// 	fmt.Printf("\n %v", info)
+		} else {
+			fmt.Printf("  -   %s\n", extIndex(ext))
 		}
 		fmt.Println("")
+	}
+}
+
+func extIndex(ext string) string {
+	switch ext {
+	case ".txt":
+		return "Text File"
+	case ".pdf":
+		return "PDF Document"
+	case ".jpg", ".jpeg", ".png", ".gif":
+		return "Image File"
+	case ".mod":
+		return "Go Module File"
+	default:
+		return "Unknown File Type"
 	}
 }
